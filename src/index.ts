72795eeff8b506a1d9d88720e0e28aa14d7fceb6
Terry Roy <terryroy@Terrys-MacBook-Pro.local>
2025-12-03 11:59:11 -0500

    cleaned up starter package

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
 // Reexport the native module. On web, it will be resolved to TapeworkAudioInterfaceModule.web.ts
 // and on native platforms to TapeworkAudioInterfaceModule.ts
 export { default } from './TapeworkAudioInterfaceModule';
-export { default as TapeworkAudioInterfaceView } from './TapeworkAudioInterfaceView';
-export * from  './TapeworkAudioInterface.types';
